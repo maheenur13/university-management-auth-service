6a43fa3486eed6fc5dbaa827122832d80b11b16f
jahidun13 <jahidun@mnessa.com>
2023-05-29 03:28:38 +0600

    allthings

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
-const port: number = 3000;
 
 app.use(cors());
 
@@ -12,13 +11,8 @@ app.use(
   })
 );
 
-// testing
-app.get("/", (req:Request, res:Response) => {
-  res.send("All things working!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get('/', (req: Request, res: Response) => {
+  res.send('All things working!');
 });
 
 export default app;
