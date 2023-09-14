d56f9dc633a0fd03b64f25f94bc57355797c9987
maheenur13 <differentmahee13@gmail.com>
2023-09-14 09:37:56 +0600

    redis added

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import app from './app';
 import config from './config/index';
 import { errLogger, logger } from './shared/logger';
+import { RedisClient } from './shared/redis';
 
 process.on('uncaughtException', error => {
   errLogger.error(error);
@@ -13,6 +14,7 @@ let server: Server;
 
 async function bootstrap() {
   try {
+    await RedisClient.connect();
     await mongoose.connect(config.db_url as string);
     logger.info(`🛢   Database is connected successfully`);
 
