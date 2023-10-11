3854d8ee2ebd7a3f45bc83987d55c9b80297dcb5
maheenur13 <differentmahee13@gmail.com>
2023-10-12 00:26:47 +0600

    solved

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 
 app.use(cors());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(
